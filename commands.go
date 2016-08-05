4a3f92bbd73d6687c2064cbc776ec4a988cb6d40
dusty <bsdpunk@gmail.com>
2016-08-05 11:54:16 -0500

    First Post

---
+++ commands.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/bsdpunk/beastietools/command"
+	"github.com/codegangsta/cli"
+)
+
+var GlobalFlags = []cli.Flag{}
+
+var Commands = []cli.Command{
+	{
+		Name:   "extip",
+		Usage:  "",
+		Action: command.CmdExtip,
+		Flags:  []cli.Flag{},
+	},
+	{
+		Name:   "profile",
+		Usage:  "",
+		Action: command.CmdProfile,
+		Flags:  []cli.Flag{},
+	},
+}
+
+func CommandNotFound(c *cli.Context, command string) {
+	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
+	os.Exit(2)
+}
